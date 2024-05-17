00d5ae9faa52382e88b139a896ad2906451c4e94
Syanda25 <siyandasec25@gmail.com>
2024-05-17 15:19:26 +0200

    Stack data structure

---
+++ StackDS.java
@@ -0,0 +1,72 @@
+/*
+ @Author: Siyanda Ntuli
+ @Email: siyandasec25@gmail.com
+ @Topic: Stack data structure (Integers)
+*/
+
+
+public class StackDS {
+    public static void main(String args[]) {
+        Stack stack = new Stack(5);
+        
+        
+    }
+}
+
+class Stack{
+    private stackSize; //Stack size (Number pf elements can be inserted)
+    private int[] stack; //Stack itself
+    private int pointer = -1;  //Pointer to point at the top of the stack
+    
+    public Stack(int stackSize){
+        this.stackSize = stackSize;
+        stack = new int[this.stackSize];
+    }
+    
+    public void push(int element){ 
+        if(isFull()){ //A
+            System.err.print("Stack overflow");
+            return;
+        }else{
+            stack[pointer+1] = element; //B
+            pointer++; //C
+        }
+    }
+    
+    public int pop(){
+        if(isEmpty()){ //Check if the stack is not empty;
+            System.err.print("Empty Stack");
+            return 0;
+        }else{
+            int value = stack[pointer];
+            pointer--;
+            return value;
+        }
+    }
+    
+    public boolean isEmpty(){
+        if(pointer<=-1){
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public boolean isFull(){
+        if(pointer+1 >= stackSize){
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public string display(){
+        String strDisplay = "[";
+        for(int element: stack){
+            strDisplay += element +","
+        }
+        strDisplay+="]";
+        
+        return strDisplay;
+    }
+}
